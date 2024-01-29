546860139b5cdd808cd490dfef09df6086b79d7b
Ethan Lim <ethanetexplorer@outlook.com>
2024-01-29 11:17:31 +0800

    Initial Commit

---
+++ Alripe/AlripeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlripeApp.swift
+//  Alripe
+//
+//  Created by Ethan Lim on 29/1/24.
+//
+
+import SwiftUI
+
+@main
+struct AlripeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Alripe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Alripe
+//
+//  Created by Ethan Lim on 29/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
