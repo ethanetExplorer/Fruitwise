c0bcfb02dd277cef4c118efe5110e83b2c915364
Ethan Lim <107737645+ethanetExplorer@users.noreply.github.com>
2024-02-23 18:31:46 +0800

    SwiftData functionality and others Moved Classes and Enums to FruitIdentificationManager fron AlripeApp Added SwiftData functionality Added functions to convert Image and UIImage to Data to be compatible with SwiftData Modified FruitItem struct to a class

---
+++ Alripe/FruitDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  FruitDetailView.swift
+//  Alripe
+//
+//  Created by Ethan Lim on 23/2/24.
+//
+
+import SwiftUI
+
+struct FruitDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FruitDetailView()
+}
